15bedf76eb4bdcb7aec27ad56d457420e7de2d7d
Michael Wood <michaelwood33311@icloud.com>
2024-05-29 10:36:20 -0400

    added displaying initial jodit editor

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { useDispatch, useSelector } from 'react-redux';
 import { counterDecrement, counterIncrement, counterSetValue } from './store/sliceCounter';
+import WysiwygAI from './components/WysiwygAI/WysiwygAI';
 
 function App() {
   const dispatch = useDispatch();
   const count = useSelector(state => state.counter)
   return (
-    <>
-      <div className='text-blue-600 cursor-pointer' onClick={() => dispatch(counterIncrement())}>+</div>
-      <div>{count}</div>
-      <div className='text-blue-600 cursor-pointer' onClick={() => dispatch(counterDecrement())}>-</div>
-      <input type="number" onChange={e => dispatch(counterSetValue(e.target.value))} />
-    </>
+    <div className='App'>
+      <WysiwygAI />
+    </div>
   )
 }
 
